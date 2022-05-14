ad4190d7e7dfaebb3f8ff42d160cd6eec53c86ad
Warre002 <warre.bossaert@gmail.com>
2022-05-14 09:30:35 +0200

    not finished

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import Register from './pages/visitors/Register';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,7 +30,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
